b5352d830e985f9ff32f7d8caa30d7ce60b97b0e
Cathero08 <ftootooo@gmail.com>
2021-12-16 01:02:26 +0800

    全新

--- gulpfile.js
+++ gulpfile.js
@@ -25,51 +25,76 @@ exports.copy = move;
 // sass編譯
 const sass = require('gulp-sass')(require('sass'));
 function sassstyle(){
-   return src('src/sass/*.scss')
+   return src('src/sass/*.scss') // 來源路徑
    .pipe(sass().on('error', sass.logError))
-   .pipe(dest('dist/css/'))
+   .pipe(dest('dist/css/')) // 目的地路徑
 }
 
-exports.style = sassstyle;
+exports.style =sassstyle;
 
 // html template
+
 const fileinclude = require('gulp-file-include');
 
 function html(){
-   return src('src/*.html')
+   return src('src/*.html') // 來源路徑
    .pipe(fileinclude({
     prefix: '@@',
     basepath: '@file'
      }))
-   .pipe(dest('./dist'));
+   .pipe(dest('./dist')); // 目的地路徑
 }
 
 exports.template = html;
 
 // js uglify
+
 const uglify = require('gulp-uglify');
 
-
-function jsmini() {
+function jsmini(){
    return src('src/js/*.js')
    .pipe(uglify())
    .pipe(dest('dist/js'))
 }
 
-exports.js = jsmini;
+exports.js =jsmini;
 
 //壓縮圖片
+
 const imagemin = require('gulp-imagemin');
 
 function min_images(){
     return src('src/images/*.*')
     .pipe(imagemin([
-        imagemin.mozjpeg({quality: 70, progressive: true}) // 壓縮品質      quality越低 -> 壓縮越大 -> 品質越差 
+        imagemin.mozjpeg({quality: 30, progressive: true}) // 壓縮品質      quality越低 -> 壓縮越大 -> 品質越差 
     ]))
     .pipe(dest('dist/images'))
 }
 
 exports.img = min_images;
+
+
+function img_copy(){
+  return src('src/images/*.*').pipe(dest('dist/images'))
+}
+
+
+
+
+
+
+
+
+// watch
+function watchall(){
+   watch(['src/*.html' , 'src/layout/*.html'] , html);
+   watch(['src/sass/*.scss' , 'src/sass/**/*.scss'] , sassstyle)
+   watch('src/js/*.js' , jsmini)
+}
+
+exports.w = watchall;
+
+
 
 
 
@@ -85,11 +110,12 @@ function browser(done) {
         },
         port: 3000
     });
-
-    watch(['src/*.html' , 'html/layout/*.html'] , html).on('change', reload);
-    watch(['src/sass/*.scss' , 'src/sass/**/*.scss'] , sassstyle).on('change', reload);
-    watch(['src/js/*.js'] , jsmini).on('change', reload);
-    done();
+     watch(['src/*.html' , 'src/layout/*.html'] , html).on('change' , reload);
+     watch(['src/sass/*.scss' , 'src/sass/**/*.scss'] , sassstyle).on('change' , reload);
+     watch('src/js/*.js' , jsmini).on('change' , reload);
+     done();
 }
 
-exports.default = browser;
+exports.default = series(browser , img_copy) ;
+
+
