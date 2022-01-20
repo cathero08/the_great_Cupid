9357035a7bc677226ee49c4b04da21d370064416
cathero08 <ftootooo@gmail.com>
2022-01-21 01:11:36 +0800

    a

--- src/API/lib/util.php
+++ src/API/lib/util.php
@@ -4,9 +4,9 @@
     function getPDO(){
 
         $db_host = "127.0.0.1";
-        $db_user = "tibamefe_since2021";
-        $db_pass = "vwRBSb.j&K#E";
-        $db_select = "tibamefe_tfd104g5";
+        $db_user = "root";
+        $db_pass = "password";
+        $db_select = "Cupid_db";
 
         //建立資料庫連線物件
         $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
@@ -21,6 +21,12 @@
     // $db_user = "tibamefe_since2021";
     // $db_pass = "vwRBSb.j&K#E";
     // $db_select = "tibamefe_tfd104g5";
+    
+    // $db_host = "127.0.0.1";
+    // $db_user = "root";
+    // $db_pass = "password";
+    // $db_select = "Cupid_db";
+
 
 
 
